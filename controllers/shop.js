95df9162fe96a509aee5ee272369f08d84ba526d
unknown <pbrid32@gmail.com>
2020-10-01 12:57:41 -0500

    creates model for cart and adds writefile functions

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
+
 // Fetch ALL products
 exports.getProducts = (req, res, next) => {
 	Product.fetchAll((products) => {
@@ -42,7 +44,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
-	console.log(prodId);
+	Product.findById(prodId, (product) => {
+		Cart.addProduct(prodId, product.price);
+	});
 	res.redirect('/cart');
 };
 // Render Orders
